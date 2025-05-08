3693bd69164dd93976449c06afdc114baad66aa9
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-08 10:29:06 +0200

    ex001-tailwind-instalar

---
+++ origamid/exercicios/ex001-tailwind-instalar/src/aula.jsx
@@ -0,0 +1,14 @@
+// - Tailwind CSS
+
+// - Documentação
+// * Use a documentação, ela é um dos motivos da fama do Tailwind.
+
+// para utilizar tailwind é essencial a extensão:  Tailwind CSS IntelliSense; ela gera um autocomplete para facilitar as customizações
+
+// https://tailwindcss.com/
+// https://tailwindcss.com/docs/installation/tailwind-cli
+
+// Para instalar, seguindo a documentação, utilizamos linha de comando (cli) e tem que dar os passos:
+// 1- npm install tailwindcss @tailwindcss/cli
+// 2- importar o tailwind no arquivo css - @import "tailwindcss";
+// 3- para buildar o tailwind cria uma pasta de output para salvar suas preferencias, então para isso tem que usar um compando cli: npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch; e no index linkar o css do output gerado por ele.
