74789b96d7b03e0f82d04fa19f03967e3674c526
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-09 16:16:51 +0200

    ex010-rounded-e-max

---
+++ origamid/exercicios/ex010-rounded-e-max/aula.jsx
@@ -0,0 +1,46 @@
+// - Rounded
+// rounded / -sm, -xl ... -full - border-radius
+
+// rounded-e / s / t / b - end / start / top / bottom
+
+// rounded-full = arredonda tudo (para fazer circulo)
+// rounded-e = end
+// rounded-s = start
+// rounded-t = top
+// rounded-b = bottom
+// rounded-bl = bottom left
+// rounded-tl = top left
+// e assim por diante...
+
+{
+  /* <button class="bg-gray-300 border-2 border-gray-800 px-4 py-2 mb-4 rounded">
+    Comprar Produto
+    </button>
+    <br />
+    <button
+    class="bg-gray-300 border-2 border-gray-800 px-4 py-2 mb-4 rounded-e-2xl"
+    >
+    Comprar Produto
+    </button>
+    <br />
+    <button
+    class="bg-gray-300 border-2 border-gray-800 px-4 py-2 mb-4 rounded-full"
+    >
+    Comprar Produto
+    </button> */
+}
+
+// - max-w (elemento cresce até max-x)
+// Utilize o max-w para controlar o tamanho máximo de width de elementos de bloco/grid/flex
+
+// A escala possui, além dos valores de 4px em 4px, medidas predefinidas como sm / lg / xl e screen-sm / screen-lg
+
+{
+  /* <h1 class="max-w-40 mx-auto bg-purple-200 text-center">
+    Cursos de Front End & UI Design
+    </h1>
+    <div class="h-8 bg-blue-200 max-w-40">max-w-40 (160px)</div>
+    <div class="h-8 bg-lime-200 max-w-sm">max-w-sm (384px)</div>
+    <div class="h-8 bg-red-200 max-w-lg">max-w-lg (512px)</div>
+    <div class="h-8 bg-yellow-200 max-w-screen-lg">max-w-screen-lg (1024px)</div> */
+}
