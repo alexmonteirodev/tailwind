e1d70c39546aca8a9b917124222e0978921e6ff7
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-09 15:41:19 +0200

    ex009-align-e-justify

---
+++ origamid/exercicios/ex009-align-e-justify/aula.jsx
@@ -0,0 +1,49 @@
+//obs: lembrar que o 'align' é omitido no tailwind, o que seria align-items, no caso é só items por exemplo.
+
+// - Align/Justify Items ( alinha todos os itens de um grid)
+
+// items (align-items) =  alinha os itens no eixo Y (vertical). Como se fossem em 3 linhas:
+
+//items-start = alinha no topo da div (primeira)
+//items-center = alinha no meio/centro da div (segunda)
+//items-end = alinha no bottom da div (terceira)
+
+// justify-items- justifica os itens no eixo X (horizontal). Como se fossem em 3 colunas:
+
+//justify-items-start = alinha na coluna da esquerda da div (primeira)
+//justify-items-center = alinha na coluna do centro da div (segunda)
+//justify-items-end = alinha na coluna da direita da div (terceira)
+
+// place-items- alinha/justifica os itens. É dizer:
+//place-items-center = items-center + justify-items-center; isso tbm vale pro start e end
+
+// start, center, end, stretch
+
+// Observação: O alinhamento/justificação só funciona quando existe espaço vazio para alinhar.
+
+// - Align/Justify Self (alinha um item individualmente)
+// self- (align) alinha o item no eixo Y (vertical)
+
+// justify-self- justifica o item no eixo X (horizontal)
+
+// place-self- alinha/justifica o item
+
+// start, center, end, stretch
+
+// Observação: O alinhamento/justificação só funciona quando existe espaço vazio para alinhar.
+
+// - Align/Justify Content
+
+// content- (align) alinha o conteúdo no eixo Y (vertical)
+
+// justify- justifica o conteúdo no eixo X (horizontal)
+
+// place-content alinha/justifica o conteúdo
+
+// start, center, end, stretch, normal, between, around
+
+//comparação dos 3:
+
+// justify-content alinha o conjunto de colunas/itens no container (horizontal)
+// justify-items alinha todos os itens dentro de suas células
+// justify-self alinha um item específico dentro de sua célula
