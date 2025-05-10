837d3b0e90fd45f3da0b700780e273be608ae4f7
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-10 10:29:41 +0200

    ex012-position

---
+++ origamid/exercicios/ex012-position/aula.jsx
@@ -0,0 +1,22 @@
+// - Position
+// absolute / relative / sticky / fixed - position
+
+// sticky = nav desce quando bate no topo ao scroll
+
+//exemplos:
+
+// class="flex justify-between bg-gray-200 p-4 fixed top-0 left-0 w-full rounded-2xl"
+
+//class="flex justify-between bg-gray-200 p-4 sticky top-0  rounded-2xl"
+
+// - top/right/bottom/left
+// alterar o posicionamento usando os seguintes valores:
+// top / right / bottom / left - valores positivos e negativos
+
+// z-0 / 10 ... 50 - z-index
+
+//obs: o elemento pai deve sempre ter o position relative para que o filho possa usar o position absolut em relação a ele.
+
+// - inset
+//inset é um atalho para o top / right / bottom / left
+// inset / inset-x - define valores para top/right/bottom/left
