de53d160afaaa98d59e7e85da2706a871622a8b4
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-10 11:35:17 +0200

    ex015-text-e-font

---
+++ origamid/exercicios/ex015-text-e-font/aula.jsx
@@ -0,0 +1,39 @@
+// - Text e Font
+// https://www.origamid.com/slide/tailwind-css/#/0302-text-e-font/2
+
+// - Escala Tipográfica
+// text-xs / sm / lg / xl / 2xl ... 9xl - font-size
+
+// - Font
+// Define estilo e peso.
+
+// font-serif / sans / mono - font-family
+
+// font-bold / light / medium / black - font-weight
+
+// italic - font-style
+
+// - Text
+// text-left / center / right - text-align
+
+// text-balance / pretty / nowrap - text-wrap
+
+// uppercase / lowercase / capitalize - text-transform
+
+// - Leading e Tracking
+// leading-normal / none / relaxed / 3 ... 10 - line-height
+
+// text-xl/relaxed - font-size + line-height
+
+// tracking-wide / tight - letter-spacing
+
+// - Underline
+// underline / line-through / no-underline - text-decoration
+
+// underline-offset-2 - text-underline-offset
+
+// decoration-gray-200 - text-decoration-color
+
+// decoration-0 / 2 ... 8 - text-decoration-thickness
+
+// decoration-dashed / wavy / dotted - text-decoration-style
