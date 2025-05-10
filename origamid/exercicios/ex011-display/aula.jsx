2e6c72b17d4172359f8477c052ebd32b21f20080
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-10 10:03:59 +0200

    ex011-display

---
+++ origamid/exercicios/ex011-display/aula.jsx
@@ -0,0 +1,25 @@
+// - Display
+// block / inline / inline-block - display: block
+
+// grid / flex / inline-flex / inline-grid - display: grid
+
+//obs:
+// inline = o tamanho do item se reduz ao tamanho do elemento. (parece estar flutuando, não ocupa bloco da tela)
+// inline = o tamanho do item se reduz ao tamanho do elemento
+// block = o tamanho do item se extende para a tela inteira, virando um bloco e ocupa um bloco da tela
+
+{
+  /*<a href="/" class="bg-blue-200 py-2 px-4 inline">Contato</a>
+    <a href="/" class="bg-blue-200 py-2 px-4 inline-block">Sobre</a>
+    <p class="bg-purple-200 py-2 px-4">Saiba Mais</p>*/
+}
+
+// - Hidden e Invisible
+// hidden - display: none;
+
+// invisible - visibility: hidden;
+
+// - sr-only
+// sr-only e not-sr-only
+
+// É uma classe utilitária que esconde o elemento da tela, mas mantém ele disponível para leitores de tela.
