cb9cef326113aaecfe21ce69e548af735dd44a73
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-10 14:19:43 +0200

    ex017-background

---
+++ origamid/exercicios/ex017-background/aula.jsx
@@ -0,0 +1,18 @@
+// - Background
+// bg-gray-200 - background-color
+
+// bg-[url('img/padrao.svg')] - valor arbitrário para usar imagens
+
+// Atenção: o caminho da url deve ser feito tomando o output.css como referencia, logo, temos: '../exercicios/ex017-background/img/padrao.svg'
+
+// - Propriedades
+// bg-cover / contain - background-size (faz ocupar o tamanho do item)
+
+// bg-no-repeat - background-repeat (nao repete a img)
+
+// bg-center / top / right / left - background-position (mostra onde a imagem começa)
+
+// bg-fixed / scroll - background-attachment (mantem a img fixa e o scroll anda pela imagem)
+
+// - Clip
+// bg-clip-text - background-clip
