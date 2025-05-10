6bbbebf6b26b11fc87f788015db9aa7f5e5f7b6d
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-10 15:09:47 +0200

    ex018-filtros

---
+++ origamid/exercicios/ex018-filtros/aula.jsx
@@ -0,0 +1,12 @@
+// - Filtros
+// Os filtros são aplicados utilizando a propriedade filter.
+
+// grayscale hover:grayscale-0 (preto e branco)
+// blur-md (embaça a img)
+// sepia (deixa em sepia - estilo foto antiga)
+//contrast-x (mexe no contraste)
+
+// - Backdrop Filter
+// Os mesmos filtros podem também ser aplicados aos elementos do fundo apenas. Lembrando que para ver os elementos do fundo, é necessário que o elemento no topo possua uma certa transparência.
+
+// backdrop-blur-md - backdrop-filter
