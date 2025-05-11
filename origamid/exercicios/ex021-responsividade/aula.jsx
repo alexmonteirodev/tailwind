d2ecfc17552291391cda4a50da743d6ff9031958
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-11 10:45:25 +0200

    ex021-responsividade

---
+++ origamid/exercicios/ex021-responsividade/aula.jsx
@@ -0,0 +1,28 @@
+// - Responsividade
+// O Tailwind possui modificadores que aplicam as propriedades de acordo com o media querie.
+
+// Ele é Mobile First, logo uma propriedade md:text-4xl será aplicada quando a tela for MAIOR que 768px já text-2xl será aplicado em qualquer tamanho de tela.
+
+// Breakpoints nativos, aplicam o @media (min-width: px):
+
+// sm: 640px
+
+// md: 768px
+
+// lg: 1024px
+
+// xl: 1280px
+
+// 2xl: 1536px
+
+//pensar assim:
+//estou codando para mobile e quero que quando o item cresca para web a partir de x (sm/md/lg...) ele faça algo
+
+// - max
+// É possível também usar o modificador max-md: para aplicar os estilos quando a tela for menor que md (768px).
+
+// Tamanhos disponíveis: sm, md, lg, xl, 2xl.
+
+// - Arbitrarios:
+// min-[] e max-[]
+// Valor arbitrário é possível com o min-[]: e max-[]:
