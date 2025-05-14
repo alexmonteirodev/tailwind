8c727bf2a9f23f571ae65a3694452b44090a8a5d
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-14 10:02:04 +0200

    ex026-transform

---
+++ origamid/exercicios/ex026-transform/aula.jsx
@@ -0,0 +1,21 @@
+// - translate
+// translate-x- - transform: translate(x,y)
+
+// translate-y- - transform: translate(x,y)
+
+//se usar sem dizer se é x ou y, ele vai pela diagonal
+
+// -rotate
+// rotate-45 - transform: rotate(45deg)
+
+// 1, 2, 45, 90, 180
+
+// - scale
+// scale-50 - transform: scale(0.5)
+
+// scale-105 - transform: scale(1.05)
+
+// Tem scale-y e scale-x para escalar em um eixo apeans.
+
+// - origin
+// origin-center / top / bottom / left / right - transform-origin: center
