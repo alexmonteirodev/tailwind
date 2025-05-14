adbc88e837a804d8f478f4fcbdd0dac59ac3e02d
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-14 13:31:48 +0200

    ex029-animacoes

---
+++ origamid/exercicios/ex029-animacoes/aula.jsx
@@ -0,0 +1,16 @@
+// - animations
+// o tailwind ja tem 4 animacoes predefinidas.
+//animate-:
+// 1- spin
+// 2- ping
+// 3- pulse
+// 4- bounce
+// 5- none
+
+//os mais famosos são:
+
+// - animate-spin
+// Anima o rotate do elemento
+
+// - animate-ping
+// Aumenta e remove a opacidade do elemento, usado em notificações.
