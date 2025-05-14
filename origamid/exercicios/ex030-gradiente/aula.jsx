3136a5ae910b2da87cfd18868e0e0892224bd711
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-14 13:52:09 +0200

    ex030-gradiente

---
+++ origamid/exercicios/ex030-gradiente/aula.jsx
@@ -0,0 +1,12 @@
+// - Gradiente
+// bg-gradient-to-b / t / tr / tl / br / bl
+//declara o gradiente e pra onde ele vai. (b- bottom; t - top e assim por diante) e depois define a cor que começa (from) e para a cor final (to). por padrão se definir a cor que começa e nao definir a cor final ele coloca transparent.
+
+//ex:
+//bg-gradient-to-b from-lime-400 to-lime-700
+
+// - Via
+// via- para adicionar cores entre o from/to
+
+// - from-%
+// É possível definir onde o background começa/termina
