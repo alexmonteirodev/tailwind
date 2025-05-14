4c8b4b04b2e869c02b1257f498ed6bf642bf63a2
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-14 09:46:21 +0200

    ex025-sombras

---
+++ origamid/exercicios/ex025-sombras/aula.jsx
@@ -0,0 +1,5 @@
+// - Sombras
+// O Tailwind oferece alguns valores padrões para sombras e permite também mudarmos a cor delas com shadow-cor
+
+// - Ring
+// ring é uma sombra utilizada para simular uma borda nos elementos. Com ela podemos criar efeitos de bordas duplas, geralmente utilizados em botões e campos de input.
