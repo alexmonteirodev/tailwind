2f31a5ed9de5e7991d38c410a0d6811de2fbc9ba
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-14 10:33:24 +0200

    ex027-transition

---
+++ origamid/exercicios/ex027-transition/aula.jsx
@@ -0,0 +1,29 @@
+// - transition
+// Anima as propriedades mais comuns como color / transform.
+
+// Usa uma duração de 150ms e uma transição com cubic-bezier (que diz que no inicio vai ter uma velociade x e no fim, y).
+
+// transition / -all / -transform / -colors / -none
+
+{
+  /*.transition {
+    transition-property: color, background-color, border-color,
+        text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter,
+        backdrop-filter;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
+    }*/
+}
+
+// - propriedades
+// duration-300 / 150 / 500 - transition-duration:
+
+// ease-linear / in / out / in-out - transition-timing-function:
+//obs: define o cubic-bezier, ou seja, in (comça rapido e termina lento); out (começa lento e termina rapido); in out (começa e termina rapido mas no meio é lento)
+
+// delay-0 / 300 / 1000 - transition-delay:
+
+// - motion-reduce
+// Para usuário que preferem utilizar aplicativos sem animações/movimentos.
+
+// motion-reduce:
