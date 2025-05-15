e203f5a6b161863d909b54b5c0f8dc107f75043b
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-15 10:55:17 +0200

    ex035-divide-e-space

---
+++ origamid/exercicios/ex035-divide-e-space/aula.jsx
@@ -0,0 +1,15 @@
+// - Space
+// Adiciona uma margem padrão entre os elementos. Usa margin da mesma forma que o grid/flex usam o gap.
+
+// space-y- margin-top/bottom
+
+// space-x- margin-right/left
+
+// - Divide
+// Adiciona bordas que dividem itens (sem adicionar ao início/final).
+
+// Atenção, se quiser um espaço padrão entre as bordas/itens utilize junto com o padding e não gap/margin.
+
+// divide-y- border-top/bottom
+
+// divide-x- border-right/left
