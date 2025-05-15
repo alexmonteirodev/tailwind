af74178bf2e37120034757333044eadaec2229d5
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-15 10:18:39 +0200

    ex033-tailwind-utility

---
+++ origamid/exercicios/ex033-tailwind-utility/aula.jsx
@@ -0,0 +1,24 @@
+// - @utility (v4)
+// Você pode adicionar qualquer estilo que não existe com o @utility
+
+// ex:
+
+// @import 'tailwindcss';
+
+// @utility text-sombra {
+//   text-shadow: 1px 1px #000;
+// }
+
+// /*
+// .text-sombra {
+//   text-shadow: 1px 1px #000;
+// }
+// */
+
+// @utility animate-delay-2 {
+//   animation-delay: 200ms;
+// }
+
+// @utility animate-delay-3 {
+//   animation-delay: 300ms;
+// }
