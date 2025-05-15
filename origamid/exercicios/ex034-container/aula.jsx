e7b3e401fc6e751579838a75a023c3bbb8ef9a76
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-15 10:33:45 +0200

    ex034-container

---
+++ origamid/exercicios/ex034-container/aula.jsx
@@ -0,0 +1,36 @@
+// - Container Classe (v4)
+// A classe .container adiciona um max-width relativo aos tamanhos de screen definidos. É possível modificar ela através do @utility. as modicações que vc criar vão somar as que ja sao pre-definidas pelo tailwind
+
+//olhar no input e html
+
+// .container {
+//   width: 100%;
+//   /* sm */
+//   @media (width >= 40rem) {
+//     max-width: 40rem;
+//   }
+//   /* md */
+//   @media (width >= 48rem) {
+//     max-width: 48rem;
+//   }
+//   /* lg */
+//   @media (width >= 64rem) {
+//     max-width: 64rem;
+//   }
+//   /* xl */
+//   @media (width >= 80rem) {
+//     max-width: 80rem;
+//   }
+//   /* 2xl */
+//   @media (width >= 96rem) {
+//     max-width: 96rem;
+//   }
+// }
+
+// @utility container {
+//   margin: 0 auto;
+//   padding: 0 0.5rem;
+//   @media (width >= 40rem) {
+//     padding: 0 1rem;
+//   }
+// }
