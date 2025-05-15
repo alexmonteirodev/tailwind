3604db83c7ea4fc805cf2139aac80b75da64f601
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-15 12:17:06 +0200

    ex037-tabelas

---
+++ origamid/exercicios/ex037-tabelas/aula.jsx
@@ -0,0 +1,6 @@
+// - Tabelas
+// border-separate / collapse - separa/colpsa as bordas dos itens da tabela
+
+// border-spacing- - adiciona um espaço entre as bordas
+
+// table-auto / fixed - tamanho proporcional ao conteúdo ou fixo
