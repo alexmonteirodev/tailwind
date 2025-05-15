2b158177bf6816836e8d176f5d35da257505f100
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-15 11:37:56 +0200

    ex036-formularios

---
+++ origamid/exercicios/ex036-formularios/aula.jsx
@@ -0,0 +1,13 @@
+// - focus
+// Remover o outline e estilizar com border/ring tornará o estilo mais consistente entre os navegadores.
+
+// Pq o outline não funciona bem nos navegadores e se v estiliza apenas o outline, ficaria inconsistente pq por exemplo no mozila ia abrir com sua config mas no chrome não, então o ideal e por outline none e configurar apenas o border e o ring.
+
+// - accent-color
+// Define a cor padrão do navegador que se extende a elementos, checkbox, radio e outros elementos.
+
+// - appearance-none
+// Remove a estilização padrão do navegador.
+
+// - file:
+// O modificador file: permite modificar o estilo do botão que permite fazer o upload de arquivos.
